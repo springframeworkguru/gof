345ce7bf31310bf6499de5357d55dbaf83cc4b0e
ximanta <ximanta.sarma@gmail.com>
2015-04-03 01:29:16 +0530

    Update BasePizzaFactory.java
    Removed the getPizza() method as addIngredients() and bakePizza() is being called from factory method createPizza() in the subclass PizzaFactory.

--- src/main/java/guru/springframework/gof/factoryMethod/BasePizzaFactory.java
+++ src/main/java/guru/springframework/gof/factoryMethod/BasePizzaFactory.java
@@ -1,11 +1,6 @@
 package guru.springframework.gof.factoryMethod;
 
 public abstract class BasePizzaFactory {
-    public Pizza getPizza(String type){
-        Pizza myPizza = this.createPizza(type);
-        myPizza.addIngredients();
-        myPizza.bakePizza();
-        return myPizza;
-    }
+    
     public abstract Pizza createPizza(String type);
 }
