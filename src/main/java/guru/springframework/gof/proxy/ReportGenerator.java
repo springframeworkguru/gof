e79cf841c286fd142fe56301f91f9de6e37147fc
Ximanta <ximanta.sarma@gmail.com>
2015-08-14 13:39:50 +0530

    Updated example code for Proxy pattern to make the use of virtual proxies more clear.

--- src/main/java/guru/springframework/gof/proxy/ReportGenerator.java
+++ src/main/java/guru/springframework/gof/proxy/ReportGenerator.java
@@ -1,6 +1,7 @@
 package guru.springframework.gof.proxy;
 
 public interface ReportGenerator {
+    void displayReportTemplate(String reportFormat,int reportEntries);
     void generateComplexReport(String reportFormat,int reportEntries);
     void generateSensitiveReport();
 }
